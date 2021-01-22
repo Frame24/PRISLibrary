fee8ad65e8d0f0714584700f2f26f22ce104069e
adse444 <Adse3014@gmail.com>
2021-01-22 13:07:35 +0300

    klbnhj

---
+++ NormPris/FormMain.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace NormPris
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.журналToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.книгиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.студентToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.группыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.направленияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.институтыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.авторыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.издательстваToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.библиотекарьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчетыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справочникиToolStripMenuItem,
+            this.отчетыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(523, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // справочникиToolStripMenuItem
+            // 
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.журналToolStripMenuItem,
+            this.книгиToolStripMenuItem,
+            this.студентToolStripMenuItem,
+            this.группыToolStripMenuItem,
+            this.направленияToolStripMenuItem,
+            this.институтыToolStripMenuItem,
+            this.авторыToolStripMenuItem,
+            this.издательстваToolStripMenuItem,
+            this.библиотекарьToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
+            // 
+            // журналToolStripMenuItem
+            // 
+            this.журналToolStripMenuItem.Name = "журналToolStripMenuItem";
+            this.журналToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.журналToolStripMenuItem.Text = "Журнал";
+            // 
+            // книгиToolStripMenuItem
+            // 
+            this.книгиToolStripMenuItem.Name = "книгиToolStripMenuItem";
+            this.книгиToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.книгиToolStripMenuItem.Text = "Книги";
+            // 
+            // студентToolStripMenuItem
+            // 
+            this.студентToolStripMenuItem.Name = "студентToolStripMenuItem";
+            this.студентToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.студентToolStripMenuItem.Text = "Студенты";
+            // 
+            // группыToolStripMenuItem
+            // 
+            this.группыToolStripMenuItem.Name = "группыToolStripMenuItem";
+            this.группыToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.группыToolStripMenuItem.Text = "Группы";
+            // 
+            // направленияToolStripMenuItem
+            // 
+            this.направленияToolStripMenuItem.Name = "направленияToolStripMenuItem";
+            this.направленияToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.направленияToolStripMenuItem.Text = "Направления";
+            // 
+            // институтыToolStripMenuItem
+            // 
+            this.институтыToolStripMenuItem.Name = "институтыToolStripMenuItem";
+            this.институтыToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.институтыToolStripMenuItem.Text = "Институты";
+            // 
+            // авторыToolStripMenuItem
+            // 
+            this.авторыToolStripMenuItem.Name = "авторыToolStripMenuItem";
+            this.авторыToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.авторыToolStripMenuItem.Text = "Авторы";
+            // 
+            // издательстваToolStripMenuItem
+            // 
+            this.издательстваToolStripMenuItem.Name = "издательстваToolStripMenuItem";
+            this.издательстваToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.издательстваToolStripMenuItem.Text = "Издательства";
+            // 
+            // библиотекарьToolStripMenuItem
+            // 
+            this.библиотекарьToolStripMenuItem.Name = "библиотекарьToolStripMenuItem";
+            this.библиотекарьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.библиотекарьToolStripMenuItem.Text = "Библиотекари";
+            // 
+            // отчетыToolStripMenuItem
+            // 
+            this.отчетыToolStripMenuItem.Name = "отчетыToolStripMenuItem";
+            this.отчетыToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.отчетыToolStripMenuItem.Text = "Отчеты";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Добро пожаловать в приложение \"Библиотека\"";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(415, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(434, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 20);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(358, 59);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 22);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Выдать книгу";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(22, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(489, 106);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Поиск книги";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 388);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Библиотека";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem группыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem направленияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem институтыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem авторыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem издательстваToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчетыToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem журналToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem книгиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem студентToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem библиотекарьToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
