8dcf178f989c2373f5227aa31def02c23edcf3f0
Frame24 <hks9@yandex.ru>
2021-01-22 15:47:04 +0500

    выа

--- NormPris/FormBook.Designer.cs
+++ NormPris/FormBook.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace NormPris
 {
-    partial class FormJournal
+    partial class FormBook
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,17 @@ namespace NormPris
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
@@ -45,18 +49,17 @@ namespace NormPris
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button7 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.button7);
             this.groupBox1.Controls.Add(this.label4);
@@ -68,14 +71,64 @@ namespace NormPris
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Location = new System.Drawing.Point(15, 364);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(460, 248);
-            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabIndex = 23;
             this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Автор";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(413, 32);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(29, 21);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "...";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Издательство";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(9, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(398, 21);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(412, 73);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(29, 21);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "...";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(8, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(398, 21);
+            this.comboBox1.TabIndex = 13;
             // 
             // button5
             // 
@@ -100,41 +153,27 @@ namespace NormPris
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(238, 144);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(110, 13);
+            this.label8.Size = new System.Drawing.Size(66, 13);
             this.label8.TabIndex = 10;
-            this.label8.Text = "Дата возврата план";
+            this.label8.Text = "Количество";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(5, 144);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.Size = new System.Drawing.Size(25, 13);
             this.label7.TabIndex = 9;
-            this.label7.Text = "Дата выдачи";
+            this.label7.Text = "Год";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(5, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Артикул";
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(8, 166);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker2.TabIndex = 4;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(241, 166);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 4;
+            this.label3.Text = "Название";
             // 
             // textBox2
             // 
@@ -149,7 +188,7 @@ namespace NormPris
             this.label2.Location = new System.Drawing.Point(12, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
-            this.label2.TabIndex = 14;
+            this.label2.TabIndex = 22;
             this.label2.Text = "Поиск";
             // 
             // button3
@@ -157,7 +196,7 @@ namespace NormPris
             this.button3.Location = new System.Drawing.Point(503, 259);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(129, 34);
-            this.button3.TabIndex = 13;
+            this.button3.TabIndex = 21;
             this.button3.Text = "Удалить";
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -166,7 +205,7 @@ namespace NormPris
             this.button2.Location = new System.Drawing.Point(503, 127);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 34);
-            this.button2.TabIndex = 12;
+            this.button2.TabIndex = 20;
             this.button2.Text = "Редактировать";
             this.button2.UseVisualStyleBackColor = true;
             // 
@@ -175,7 +214,7 @@ namespace NormPris
             this.button1.Location = new System.Drawing.Point(503, 87);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 34);
-            this.button1.TabIndex = 11;
+            this.button1.TabIndex = 19;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -185,81 +224,43 @@ namespace NormPris
             this.dataGridView1.Location = new System.Drawing.Point(12, 87);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(463, 271);
-            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.TabIndex = 18;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 61);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(463, 20);
-            this.textBox1.TabIndex = 9;
+            this.textBox1.TabIndex = 17;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Справочник \"Журналы\"";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Справочник \"Книги\"";
             // 
-            // comboBox1
+            // numericUpDown1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(8, 73);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(398, 21);
-            this.comboBox1.TabIndex = 13;
+            this.numericUpDown1.Location = new System.Drawing.Point(241, 166);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(200, 20);
+            this.numericUpDown1.TabIndex = 24;
             // 
-            // button6
+            // textBox3
             // 
-            this.button6.Location = new System.Drawing.Point(412, 73);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(29, 21);
-            this.button6.TabIndex = 14;
-            this.button6.Text = "...";
-            this.button6.UseVisualStyleBackColor = true;
+            this.textBox3.Location = new System.Drawing.Point(9, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(197, 20);
+            this.textBox3.TabIndex = 25;
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(5, 57);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 13);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "Студент";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(9, 32);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(398, 21);
-            this.comboBox2.TabIndex = 13;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(413, 32);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(29, 21);
-            this.button7.TabIndex = 14;
-            this.button7.Text = "...";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 16);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 13);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "Книга";
-            // 
-            // FormJournal
+            // FormBook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 665);
+            this.ClientSize = new System.Drawing.Size(664, 628);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
@@ -268,11 +269,12 @@ namespace NormPris
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Name = "FormJournal";
-            this.Text = "Журнал";
+            this.Name = "FormBook";
+            this.Text = "Книга";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -281,13 +283,19 @@ namespace NormPris
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
@@ -296,11 +304,5 @@ namespace NormPris
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
