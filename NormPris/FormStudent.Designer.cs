bb2dd02370cce27f9303b8c8f22b6e718fe68a74
adse444 <Adse3014@gmail.com>
2021-01-22 13:19:54 +0300

    прыва

--- NormPris/FormStudent.Designer.cs
+++ NormPris/FormStudent.Designer.cs
@@ -49,6 +49,8 @@ namespace NormPris
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -79,27 +81,27 @@ namespace NormPris
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(503, 87);
+            this.button1.Location = new System.Drawing.Point(503, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 24);
+            this.button1.Size = new System.Drawing.Size(129, 34);
             this.button1.TabIndex = 3;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(503, 117);
+            this.button2.Location = new System.Drawing.Point(503, 112);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 24);
+            this.button2.Size = new System.Drawing.Size(129, 34);
             this.button2.TabIndex = 4;
             this.button2.Text = "Редактировать";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(503, 175);
+            this.button3.Location = new System.Drawing.Point(503, 152);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(129, 24);
+            this.button3.Size = new System.Drawing.Size(129, 34);
             this.button3.TabIndex = 5;
             this.button3.Text = "Удалить";
             this.button3.UseVisualStyleBackColor = true;
@@ -115,6 +117,8 @@ namespace NormPris
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label6);
@@ -127,7 +131,7 @@ namespace NormPris
             this.groupBox1.Controls.Add(this.textBox4);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Location = new System.Drawing.Point(14, 380);
+            this.groupBox1.Location = new System.Drawing.Point(15, 382);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(460, 270);
             this.groupBox1.TabIndex = 7;
@@ -136,23 +140,23 @@ namespace NormPris
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(58, 63);
+            this.textBox2.Location = new System.Drawing.Point(9, 56);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
             this.textBox2.TabIndex = 0;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(58, 89);
+            this.textBox3.Location = new System.Drawing.Point(9, 96);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(200, 20);
             this.textBox3.TabIndex = 1;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(58, 115);
+            this.textBox4.Location = new System.Drawing.Point(9, 148);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(200, 20);
             this.textBox4.TabIndex = 2;
             // 
             // comboBox1
@@ -160,19 +164,19 @@ namespace NormPris
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(242, 56);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(242, 109);
+            this.dateTimePicker1.Location = new System.Drawing.Point(242, 145);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 4;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(242, 83);
+            this.dateTimePicker2.Location = new System.Drawing.Point(242, 96);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker2.TabIndex = 4;
@@ -180,56 +184,74 @@ namespace NormPris
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 66);
+            this.label3.Location = new System.Drawing.Point(6, 40);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "ФИО";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 90);
+            this.label4.Location = new System.Drawing.Point(6, 80);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(161, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
+            this.label4.Text = "Номер студентческого билета";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 118);
+            this.label5.Location = new System.Drawing.Point(6, 123);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(156, 13);
             this.label5.TabIndex = 7;
-            this.label5.Text = "label5";
+            this.label5.Text = "Номер читательского билета";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(201, 59);
+            this.label6.Location = new System.Drawing.Point(239, 40);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(42, 13);
             this.label6.TabIndex = 8;
-            this.label6.Text = "label6";
+            this.label6.Text = "Группа";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(201, 86);
+            this.label7.Location = new System.Drawing.Point(239, 80);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(153, 13);
             this.label7.TabIndex = 9;
-            this.label7.Text = "label7";
+            this.label7.Text = "Дата студентческого билета";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(201, 115);
+            this.label8.Location = new System.Drawing.Point(239, 123);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(148, 13);
             this.label8.TabIndex = 10;
-            this.label8.Text = "label8";
+            this.label8.Text = "Дата читательского билета";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(205, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 27);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Ок";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(317, 216);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 27);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Отмена";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // FormStudent
             // 
@@ -276,5 +298,7 @@ namespace NormPris
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
     }
 }
